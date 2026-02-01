175582d968a6717dfbe44c03d391c173fb6205ce
deivix13 <deividcode13@gmail.com>
2026-02-01 11:45:54 +0100

    delete hardcoded condiguration

--- producer-apache-kafka/src/main/java/com/deivix/example/config/KafkaProducerConfig.java
+++ producer-apache-kafka/src/main/java/com/deivix/example/config/KafkaProducerConfig.java
@@ -3,13 +3,11 @@ package com.deivix.example.config;
 import com.deivix.example.model.TransactionMessage;
 import lombok.NonNull;
 import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.UUIDSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
-import org.springframework.kafka.support.serializer.JacksonJsonSerializer;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -26,22 +24,21 @@ public class KafkaProducerConfig {
 
         // Address of the Kafka broker the producer will connect to
         configProps.put(
-                ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
-                "localhost:9092"
+                ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "${spring.kafka.producer.bootstrap-servers}"
         );
 
         // Serializer used to convert the message KEY (UUID) into bytes
         // Kafka can only send byte[], so this is mandatory
         configProps.put(
                 ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
-                UUIDSerializer.class
+                "${spring.kafka.producer.key-serializer:org.apache.kafka.common.serialization.UUIDSerializer}"
         );
 
         // Serializer used to convert the message VALUE (TransactionMessage)
         // into JSON and then into bytes before sending to Kafka
         configProps.put(
                 ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
-                JacksonJsonSerializer.class
+                "${spring.kafka.producer.value-serializer:org.springframework.kafka.support.serializer.JsonSerializer}"
         );
 
         // Creates a ProducerFactory with the given configuration.
