49a74d84c51b541ef89a8e1c8f594cb471bcc741
deivix13 <deividcode13@gmail.com>
2026-02-01 14:25:35 +0100

    overall improvement

--- consumer-apache-kafka/src/main/java/com/deivix/example/config/KafkaConsumerConfig.java
+++ consumer-apache-kafka/src/main/java/com/deivix/example/config/KafkaConsumerConfig.java
@@ -4,6 +4,7 @@ import com.deivix.example.model.TransactionMessage;
 import lombok.NonNull;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.UUIDDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -20,29 +21,29 @@ import java.util.UUID;
 @Configuration
 public class KafkaConsumerConfig {
 
-    /**
-     * Defines how Kafka consumers are created.
-     * This includes broker address, consumer group and deserializers.
-     */
+    @Value("${spring.kafka.consumer.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Value("${spring.kafka.consumer.group-id}")
+    private String groupId;
+
+
+    // Defines how Kafka consumers are created.
     @Bean
     public ConsumerFactory<@NonNull UUID, @NonNull TransactionMessage> consumerFactory() {
         Map<String, Object> props = new HashMap<>();
 
-        // Kafka broker address
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, this.bootstrapServers);
 
-        // Consumer group id
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "AA-demo");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, this.groupId);
 
         // Wrap deserializers to handle errors without crashing the consumer
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
 
-        // Actual deserializers used internally
         props.put(ErrorHandlingDeserializer.KEY_DESERIALIZER_CLASS, UUIDDeserializer.class);
         props.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, JacksonJsonDeserializer.class);
 
-        // JSON -> TransactionMessage configuration
         props.put(JacksonJsonDeserializer.USE_TYPE_INFO_HEADERS, false);
         props.put(JacksonJsonDeserializer.VALUE_DEFAULT_TYPE,
                 "com.deivix.example.model.TransactionMessage");
